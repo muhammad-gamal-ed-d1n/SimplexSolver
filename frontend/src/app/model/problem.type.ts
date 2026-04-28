a4c1cdeebd23315bb2a00e240d6f38723274e293
muhammad-gamal-ed-d1n <muhammadgamaleddin@gmail.com>
2026-04-28 17:21:04 +0300

    initial basis

---
+++ frontend/src/app/model/problem.type.ts
@@ -0,0 +1,3 @@
+enum ProblemType {
+    MAX, MIN
+}
--- frontend/src/app/services/engine.ts
+++ frontend/src/app/services/engine.ts
@@ -4,5 +4,56 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Engine {
-  
+  private constraints: Constraints[] = []
+  private equations: number[][] = []
+  private objective: number[] = []
+  private rhs: number[] = []
+  private basis: string[] = []
+  private type!: ProblemType;
+  private nConstraints!: number;
+  private nVariables!: number;
+
+  public simplex() {
+    // this is done to unify the logic of min and max problems, by turning a min problem into a max one
+    this.flipSignOfObjfn();
+    // TODO: check if it is already optimized
+
+  }
+
+  private flipSignOfObjfn() {
+    for (let i = 0; i < this.nVariables; i++) {
+      this.objective[i] *= -1;
+    }
+  }
+
+  private standardize() {
+    for (let i = 0; i < this.nConstraints; i++) {
+      if (this.constraints[i] == Constraints.LESSTHANOREQUAL) { // add surplus
+        // add a variable equal to zero to all equations
+        this.addVariable();
+        // set surplus coefficient to one
+        this.equations[i][this.nVariables - 1] = 1;
+      }
+      else if (this.constraints[i] == Constraints.GREATERTHANOREQUAL) {
+        // TODO: add artificials and implement two phase simplex
+      }
+    }
+  }
+
+  private addVariable() {
+    for (let i = 0; i <  this.nConstraints; i++) {
+      this.equations[i] = [...this.equations[i], 0];
+    }
+
+    this.objective = [...this.objective, 0];
+    this.nVariables++;
+  }
+
+  private isOptimized() {
+    for (let num of this.objective) {
+      // return false if any entry is less than zero
+      if (num < 0) return false;
+    }
+    return true;
+  }
 }
